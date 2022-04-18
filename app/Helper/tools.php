9468ab0a6d0084eecb51d8fa1f0ef9c86ad4444c
“张桂福” <904824669@qq.com>
2022-04-18 15:42:54 +0800

    加入编辑图片函数

--- app/Helper/tools.php
+++ app/Helper/tools.php
@@ -28,3 +28,57 @@ function msg($code, $msg) {
 
     return json_encode($result, JSON_UNESCAPED_UNICODE);
 }
+function compressedImage($imgsrc, $imgdst) {
+    list($width, $height, $type) = getimagesize($imgsrc);
+
+    $new_width = $width;//压缩后的图片宽
+    $new_height = $height;//压缩后的图片高
+
+    if($width >= 600){
+        $per = 600 / $width;//计算比例
+        $new_width = $width * $per;
+        $new_height = $height * $per;
+    }
+
+    switch ($type) {
+        case 1:
+            $giftype = check_gifcartoon($imgsrc);
+            if ($giftype) {
+                header('Content-Type:image/gif');
+                $image_wp = imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefromgif($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                //90代表的是质量、压缩图片容量大小
+                imagejpeg($image_wp, $imgdst, 90);
+                imagedestroy($image_wp);
+                imagedestroy($image);
+
+                return 0;
+            }
+            break;
+        case 2:
+//            header('Content-Type:image/jpeg');
+            $image_wp = imagecreatetruecolor($new_width, $new_height);
+            $image = imagecreatefromjpeg($imgsrc);
+            imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+            //90代表的是质量、压缩图片容量大小
+            imagejpeg($image_wp, $imgdst, 90);
+            imagedestroy($image_wp);
+            imagedestroy($image);
+
+            return 0;
+            break;
+        case 3:
+            header('Content-Type:image/png');
+            $image_wp = imagecreatetruecolor($new_width, $new_height);
+            $image = imagecreatefrompng($imgsrc);
+            imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+            //90代表的是质量、压缩图片容量大小
+            imagejpeg($image_wp, $imgdst, 90);
+            imagedestroy($image_wp);
+            imagedestroy($image);
+
+            return 0;
+            break;
+    }
+}
