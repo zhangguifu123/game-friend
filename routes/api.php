4773628cead649bb2848cec77a099d101a353caa
张桂福 <904824669@qq.com>
2022-04-18 13:31:43 +0800

    api调整

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Game\GameController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,12 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::namespace('Api')->group(function (){
     /**Game */
     Route::post('/test','Game\GameController@publish');
     //Game增删改查
-    Route::post('/game/add','Game\GameController@publish');
+    Route::post('/game/add',[GameController::class, 'publish']);
     Route::get('/game/me/{uid}','Game\GameController@get_me_list');
     Route::get('/game/like/{uid}','Game\GameController@get_like_list');
     Route::get('/game/collection/{uid}','Game\GameController@get_collection_list');
