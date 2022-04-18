87eb3a88eaa10374ca0c1118efa049866d65413d
“张桂福” <904824669@qq.com>
2022-04-18 12:20:59 +0800

    game测试

--- database/migrations/2022_04_15_060521_create_games_table.php
+++ database/migrations/2022_04_15_060521_create_games_table.php
@@ -15,6 +15,16 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->string('publisher')->comment('发布者')->index();
+            $table->string('name')->comment('赛事名称');
+            $table->string('sign_up_time')->comment('报名时间');
+            $table->string('game_time')->comment('比赛时间');
+            $table->string('organizer')->comment('主办单位');
+            $table->string('subject')->comment('竞赛科目');
+            $table->string('type')->comment('竞赛类别');
+            $table->string('level')->comment('竞赛级别');
+            $table->integer("collections")->default(0)->comment("被收藏次数");
+            $table->string('img')->comment('图片链接');
             $table->timestamps();
         });
     }
--- database/migrations/2022_04_15_060630_create_managers_table.php
+++ database/migrations/2022_04_15_060630_create_managers_table.php
@@ -15,6 +15,11 @@ class CreateManagersTable extends Migration
     {
         Schema::create('managers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('phone')->unique()->comment('电话');
+            $table->string('password')->comment('密码');
+            $table->string('department')->comment('部门');
+            $table->integer('level')->comment('管理员级别');
             $table->timestamps();
         });
     }
