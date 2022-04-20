390883059865a0683393f733a0962db591c1c62c
“张桂福” <904824669@qq.com>
2022-04-20 18:01:29 +0800

    中间键manage

--- app/Http/Controllers/User/FriendController.php
+++ app/Http/Controllers/User/FriendController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\User\Friend;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FriendController extends Controller
 {
