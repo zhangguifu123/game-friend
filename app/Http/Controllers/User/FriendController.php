bef4ff726002b11b48761cc063914a39c46268f9
“张桂福” <904824669@qq.com>
2022-04-24 14:33:21 +0800

    评论区

--- app/Http/Controllers/User/FriendController.php
+++ app/Http/Controllers/User/FriendController.php
@@ -46,7 +46,7 @@ class FriendController extends Controller
             return msg(3 , __LINE__);
         }
         $Notices = Friend::query()->where('friend_id', $request->route('uid'))->where('status', 0)->get('user_id')->toArray();
-        $friends = User::query()->whereIn('openid', $Notices)->get(['id','name','avatar'])->toArray();
+        $friends = User::query()->whereIn('openid', $Notices)->get(['openid','name','avatar'])->toArray();
         return msg(0, $friends);
     }
     //确认申请
