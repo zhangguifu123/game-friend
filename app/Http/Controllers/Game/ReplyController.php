97e3001ac75e4e86f5c6d66d3779fad93d122fa9
“张桂福” <904824669@qq.com>
2022-04-26 10:02:44 +0800

    评论区

--- app/Http/Controllers/Game/ReplyController.php
+++ app/Http/Controllers/Game/ReplyController.php
@@ -74,6 +74,9 @@ class ReplyController extends Controller
     {
         $reply = Reply::query()->find($request->route('id'));
         // 将该评测从我的发布中删除
+        if (!$reply){
+            return msg(3, __LINE__);
+        }
         $reply->delete();
         return msg(0, __LINE__);
     }
