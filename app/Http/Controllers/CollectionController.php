a8cbb3884d1f0089d2a38ea83e709892fb30c62e
“张桂福” <904824669@qq.com>
2022-05-02 12:51:54 +0800

    s

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -99,11 +99,9 @@ class CollectionController extends Controller
         }
         $gameList = Post::query()->whereIn('posts.id',$postIds)
             ->leftJoin('users','posts.publisher','=','users.openid')
-            ->get(
-//                [
-//                "posts.id", "publisher", "users.avatar", "posts.name", "level", "theme", "title" ,"content","img", "views", "posts.created_at", "collections"
-//            ]
-            )
+            ->get([
+                "posts.id", "publisher", "users.avatar", "posts.name", "level", "theme", "title" ,"content","img", "views", "posts.created_at", "collections", "phone"
+            ])
             ->toArray();
         return msg(0, $gameList);
     }
