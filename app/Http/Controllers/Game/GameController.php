d3d0435ce394ed5eb249d0da0bbf8e47ff0be5d1
“张桂福” <904824669@qq.com>
2022-05-09 21:49:03 +0800

    s

--- app/Http/Controllers/Game/GameController.php
+++ app/Http/Controllers/Game/GameController.php
@@ -29,7 +29,7 @@ class GameController extends Controller
     /** 拉取列表信息 */
     public function getList(Request $request)
     {
-        print_r($request->input('subject'));
+        var_dump($request->input('subject'));
         if (!$request->input('uid') || is_null($request->input('subject')) || is_null($request->input('level'))){
             return msg(11, __LINE__);
         }
