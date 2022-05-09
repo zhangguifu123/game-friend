e6e862cbe479c666d9090fc8f55c3aa97beba2f3
“张桂福” <904824669@qq.com>
2022-05-09 20:09:41 +0800

    s

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -38,12 +38,11 @@ class CollectionController extends Controller
         if (!$request->route('id')) {
             return msg(3 , __LINE__);
         }
-        $worker   = GameCollection::query()->where('uid', $request->route('id'))->get()->toArray();
-        $workerIds = [];
-        foreach ($worker as $value){
-            $workerIds[] = $value['game_id'];
-        }
-        $gameList = Game::query()->whereIn('games.id',$workerIds)->get()->toArray();
+        $uid = $request->route('id');
+        $gameList   = GameCollection::query()->where('uid', $uid)
+            ->leftJoin('games','games.id', '=', 'game_collections.game_id')
+            ->get()
+            ->toArray();
         return msg(0, $gameList);
     }
 
